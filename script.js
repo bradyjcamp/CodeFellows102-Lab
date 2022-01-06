42ab4262ba6907b0d6742569ae3a9fca92c63f74
Brady Camp <bradyjcamp@gmail.com>
2022-01-06 17:03:50 -0500

    edits on js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function clicked() {
 document.getElementById('btn').addEventListener('click', clicked);
 
 function submit() {
-    var favorite = document.getElementById('favorite_tree').value;
-    var name = document.getElementById('why').value;
+    let favorite = document.getElementById('favorite_tree').value;
+    let name = document.getElementById('why').value;
  
-    var html = 'That is awesome! I love the <b>' + favorite + ' too! Thank you for sharing </b> ' + name;
+    let html = 'That is awesome! I love the <b>' + favorite + ' too! Thank you for sharing </b> ' + name;
  
     document.getElementById('result').innerHTML = html;
 }
@@ -23,5 +23,6 @@ if (words == 'Yes' || 'yes' || 'Yes!' || 'yes!' || 'YES' || 'YES!' || 'heck ya!'
 else { 
     confirm('Uh oh! You must love trees to enter!')
 }
+
 
 
