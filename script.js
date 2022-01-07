0740a59777664e472e33be5d13ee6f0d440a4cfc
Brady Camp <bradyjcamp@gmail.com>
2022-01-06 20:58:57 -0500

    working on JS loops

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('btn').addEventListener('click', clicked);
 
 function submit() {
     let favorite = document.getElementById('favorite_tree').value;
-    let name = document.getElementById('why').value;
+    let name = document.getElementById('what_name').value;
  
     let html = 'That is awesome! I love the <b>' + favorite + ' too! Thank you for sharing </b> ' + name;
  
@@ -15,14 +15,12 @@ function submit() {
  
 document.getElementById('respond').addEventListener('click', submit);
 
-let words = prompt('Do you love trees?');
+let likeTrees = confirm('Do you love trees?');
 
-if (words == 'Yes' || 'yes' || 'Yes!' || 'yes!' || 'YES' || 'YES!' || 'heck ya!'){
-    confirm('Yay me too! Come on in!');
-}
-else { 
-    confirm('Uh oh! You must love trees to enter!')
+while(!likeTrees) {
+likeTrees = confirm('Do you love trees?');
 }
 
+let howMany = parseInt(prompt('How many trees would you like to see? 1-3'));
 
-
+   
